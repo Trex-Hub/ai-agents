dbf9785e942185b93c145f1780c74630ae273ea7
Devendra Mishra <devendra@mavonic.com>
2025-04-25 20:02:38 +0530

    feat(system-prompt): improved system prompt moved generic stuff to constant

--- src/mastra/agents/slack.ts
+++ src/mastra/agents/slack.ts
@@ -1,18 +1,11 @@
-import { groq } from '@ai-sdk/groq';
 import { Agent } from '@mastra/core/agent';
 import { mcp } from '../mcp/slack';
+import { model } from '../../utils/constant';
+import { slackAgentInstructions } from '../instructions/slack-agent';
 
 export const slackAgent = new Agent({
   name: 'Slack Agent',
-  instructions: `
-      You are a helpful slack assistant that provides accurate slack information.
-
-      Your primary function is to help users get slack details for specific locations. When responding:
-      - Always ask for a location if none is provided
-      - If the location name isn't in English, please translate it
-      - If giving a location with multiple parts (e.g. "New York, NY"), use the most relevant part (e.g. "New York")
-      - Include relevant details like humidity, wind conditions, and precipitation
-      - Keep responses concise but informative`,
-  model: groq('llama-3.3-70b-versatile'),
+  instructions: slackAgentInstructions,
+  model,
   tools: await mcp.getTools(), // Tools are fixed at agent creation
 });
